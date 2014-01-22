5d7d24a12c7b18fc86ba10bb0efba7fc77107888
Ubuntu <ubuntu@ip-10-190-90-35.ec2.internal>
2014-01-22 19:52:50 +0000

    finally figured out how deploy works

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:callowaylc/gw-api.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/api'
+set :deploy_to, '/home/ubuntu/Develop/gw-api'
 
 # Default value for :scm is :git
 set :scm,    :git
